5cebecb07505d80e0ecbe17d7ab509e19267d065
aminulislamrahat <aminulislamrahat005@gmail.com>
2025-06-27 05:28:46 +0600

    recent data limit 8

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run () {
       const result = await projectCollection
         .find()
         .sort({ deadline: 1 })
-        .limit(6)
+        .limit(8)
         .toArray()
       res.send(result)
     })
